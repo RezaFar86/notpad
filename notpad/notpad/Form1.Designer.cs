917de5821253c4ee1caaee869f6ab74f7b14e1d2
RezaFar86 <rezafaraj86@gmail.com>
2024-11-20 18:06:52 +0330

    Add files via upload

---
+++ notpad/notpad/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace notpad
+{
+    partial class frmmain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.فایلToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.جدیدToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.بازکردنToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ذخیرهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.خروجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تنظیماتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تنظیماترنگبرنامهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تنظیماترنگفونتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تنظیماتخودفونتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.راهنماToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtmain = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(111, 17);
+            this.toolStripStatusLabel1.Text = "امروز:چهارشنبه 30آبان";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.فایلToolStripMenuItem,
+            this.تنظیماتToolStripMenuItem,
+            this.راهنماToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // فایلToolStripMenuItem
+            // 
+            this.فایلToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.جدیدToolStripMenuItem,
+            this.بازکردنToolStripMenuItem,
+            this.ذخیرهToolStripMenuItem,
+            this.خروجToolStripMenuItem});
+            this.فایلToolStripMenuItem.Name = "فایلToolStripMenuItem";
+            this.فایلToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.فایلToolStripMenuItem.Text = "فایل";
+            // 
+            // جدیدToolStripMenuItem
+            // 
+            this.جدیدToolStripMenuItem.Name = "جدیدToolStripMenuItem";
+            this.جدیدToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.جدیدToolStripMenuItem.Text = "جدید";
+            this.جدیدToolStripMenuItem.Click += new System.EventHandler(this.جدیدToolStripMenuItem_Click);
+            // 
+            // بازکردنToolStripMenuItem
+            // 
+            this.بازکردنToolStripMenuItem.Name = "بازکردنToolStripMenuItem";
+            this.بازکردنToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.بازکردنToolStripMenuItem.Text = "بازکردن";
+            this.بازکردنToolStripMenuItem.Click += new System.EventHandler(this.بازکردنToolStripMenuItem_Click_1);
+            // 
+            // ذخیرهToolStripMenuItem
+            // 
+            this.ذخیرهToolStripMenuItem.Name = "ذخیرهToolStripMenuItem";
+            this.ذخیرهToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ذخیرهToolStripMenuItem.Text = "ذخیره";
+            this.ذخیرهToolStripMenuItem.Click += new System.EventHandler(this.ذخیرهToolStripMenuItem_Click);
+            // 
+            // خروجToolStripMenuItem
+            // 
+            this.خروجToolStripMenuItem.Name = "خروجToolStripMenuItem";
+            this.خروجToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.خروجToolStripMenuItem.Text = "خروج";
+            // 
+            // تنظیماتToolStripMenuItem
+            // 
+            this.تنظیماتToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تنظیماترنگبرنامهToolStripMenuItem,
+            this.تنظیماترنگفونتToolStripMenuItem,
+            this.تنظیماتخودفونتToolStripMenuItem});
+            this.تنظیماتToolStripMenuItem.Name = "تنظیماتToolStripMenuItem";
+            this.تنظیماتToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.تنظیماتToolStripMenuItem.Text = "تنظیمات";
+            // 
+            // تنظیماترنگبرنامهToolStripMenuItem
+            // 
+            this.تنظیماترنگبرنامهToolStripMenuItem.Name = "تنظیماترنگبرنامهToolStripMenuItem";
+            this.تنظیماترنگبرنامهToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.تنظیماترنگبرنامهToolStripMenuItem.Text = "تنظیمات رنگ برنامه";
+            this.تنظیماترنگبرنامهToolStripMenuItem.Click += new System.EventHandler(this.تنظیماترنگبرنامهToolStripMenuItem_Click);
+            // 
+            // تنظیماترنگفونتToolStripMenuItem
+            // 
+            this.تنظیماترنگفونتToolStripMenuItem.Name = "تنظیماترنگفونتToolStripMenuItem";
+            this.تنظیماترنگفونتToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.تنظیماترنگفونتToolStripMenuItem.Text = "تنظیمات رنگ فونت";
+            this.تنظیماترنگفونتToolStripMenuItem.Click += new System.EventHandler(this.تنظیماترنگفونتToolStripMenuItem_Click);
+            // 
+            // تنظیماتخودفونتToolStripMenuItem
+            // 
+            this.تنظیماتخودفونتToolStripMenuItem.Name = "تنظیماتخودفونتToolStripMenuItem";
+            this.تنظیماتخودفونتToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.تنظیماتخودفونتToolStripMenuItem.Text = "تنظیمات خود فونت";
+            this.تنظیماتخودفونتToolStripMenuItem.Click += new System.EventHandler(this.تنظیماتخودفونتToolStripMenuItem_Click);
+            // 
+            // راهنماToolStripMenuItem
+            // 
+            this.راهنماToolStripMenuItem.Name = "راهنماToolStripMenuItem";
+            this.راهنماToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.راهنماToolStripMenuItem.Text = "راهنما";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 30);
+            this.panel1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(185, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "پاک کردن همه";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(104, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ذخیره";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtmain
+            // 
+            this.txtmain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtmain.Location = new System.Drawing.Point(0, 54);
+            this.txtmain.Multiline = true;
+            this.txtmain.Name = "txtmain";
+            this.txtmain.Size = new System.Drawing.Size(284, 185);
+            this.txtmain.TabIndex = 5;
+            // 
+            // frmmain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtmain);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmmain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "صفحه اصلی";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmmain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem فایلToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem جدیدToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem بازکردنToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ذخیرهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem خروجToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تنظیماتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تنظیماترنگبرنامهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تنظیماترنگفونتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تنظیماتخودفونتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem راهنماToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtmain;
+    }
+}
+
---
+++ notpad/notpad/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notpad
+{
+    public partial class frmmain : Form
+    {
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void خروجToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmmain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void بازکردنToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ذخیرهکردنToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ذخیرهToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void بازکردنToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.ShowDialog();
+            txtmain.Text = System.IO.File.ReadAllText(o.FileName);
+        }
+
+        private void تنظیماترنگبرنامهToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            c.ShowDialog();
+            txtmain.BackColor = c.Color;
+        }
+
+        private void تنظیماترنگفونتToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog c = new ColorDialog();
+            c.ShowDialog();
+            txtmain.ForeColor = c.Color;
+        }
+
+        private void تنظیماتخودفونتToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog f = new FontDialog();
+            f.ShowDialog();
+            txtmain.Font = f.Font;
+        }
+
+        private void txtmain_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.ShowDialog();
+            System.IO.File.WriteAllText(s.FileName, txtmain.Text);
+        }
+
+        private void جدیدToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
